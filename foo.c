7069971c983233b5c47f7e25203136ac8698c94c
Vali <valerie.wartscher@gmail.com>
2025-12-18 12:04:55 +0000

    step 2

--- foo.c
+++ foo.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#import math 
 
 int main(void)
 {
     printf("%s\n", "Hallo Welt!");
 
     printf("Aufgabe 1 Weihnachtskekse");
-    
+    number_biscuts =int(input("5"));
+    daimeter_biscuts =float(input("4"));
+    radius = diameter_biscuts / 2;
+    area_per_biscuts = math.pi * radius ** 2;
+    total_area = diameter 
+
+
+
 
 }
